4607059752bdb58e450a0b21d4c7a111cbf5ad12
Felipe Ehara <felipeyoug1@gmail.com>
2022-10-25 22:10:55 -0300

    adicionando arquivos

---
+++ etapas.js
@@ -0,0 +1,49 @@
+let etapas = [
+    {
+        titulo: 'VEREADOR',
+        numeros: 5,
+        candidatos: [
+            {
+                numero: '38111',
+                nome: 'Fulano de Tal',
+                partido: 'ABC',
+                fotos: [
+                    {url: 'gabriel-monteiro.jpg', legenda: 'Vereador', small: false}
+                ]
+            },
+            {
+                numero: '77222',
+                nome: ' Beltrano da Silva',
+                partido: 'DEFG',
+                fotos: [
+                    {url:'renato-freitads.jpg', legenda: 'vereador'}
+                ]
+            },
+        ]
+    },
+    {
+        titulo: 'PREFEITO',
+        numeros: 2,
+        candidatos: [
+            {
+                numero: '99',
+                nome: 'Ciclano',
+                partido:'ABC',
+                fotos: [
+                    {url: 'bolsonaro 22.jpg', legenda: 'Prefeito'},
+                    {url: 'braganetto2.jpg', legenda: 'Vice-Prefeito', small: true}
+                ]
+            },
+            {
+                numero: '84',
+                nome: 'Zulano',
+                partido: 'QWERTY',
+                vice: 'Zul',
+                fotos: [
+                    {url: 'lula.jpeg', legenda: 'Prefeito'},
+                    {url: 'geraldo-alckmin.jpeg', legenda: 'Vice-prefeito', small: true}
+                ]
+            },
+        ]
+    }
+];
